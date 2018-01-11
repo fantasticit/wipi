e25f938e48b015735addee14a8e36f6126d2e459
mvpzx <bken2016@163.com>
2018-01-11 23:15:38 +0800

    modify model user

--- server/models/user.js
+++ server/models/user.js
@@ -4,7 +4,11 @@ const Schema = mongoose.Schema
 const userSchema = new Schema({
   id: Number,
   account: String,
-  password: String
+  password: String,
+  roles: {
+    type: Array,
+    default: []
+  }
 })
 
 userSchema.index({ id: 1 })
