e48542dced0b3d32a551204a46949be3b275d7ff
zhaoxu <bken2016@163.com>
2018-01-26 10:56:19 +0800

    update router

--- server/router/index.js
+++ server/router/index.js
@@ -1,6 +1,7 @@
 const router = require('koa-router')()
 const UserController = require('../controller/user')
 const ArticleController = require('../controller/article')
+const QiniuController = require('../controller/qiniu')
 
 module.exports = (app) => {
   router.post('/user/register', UserController.register)
@@ -8,6 +9,8 @@ module.exports = (app) => {
 
   router.get('/article', ArticleController.get)
   router.post('/article/new', ArticleController.add)
+
+  router.get('/qiniu/token', QiniuController.getQiniuToken)
 
   app.use(router.routes())
     .use(router.allowedMethods())
