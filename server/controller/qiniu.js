b0f1333359a60fab1e5ca22e3ffd08fa5371ac72
mvpzx <bken2016@163.com>
2018-01-27 22:45:20 +0800

    update qiniu

--- server/controller/qiniu.js
+++ server/controller/qiniu.js
@@ -4,14 +4,13 @@ const secretKey = '2Rr3QAZd79rb_SnAr6wb2cVmaTo6MVM-_r-Ocn_6'
 const scope = 'coding-heart'
 const expires = 7200
 const options = { scope, expires }
+const mac = new qiniu.auth.digest.Mac(accessKey, secretKey)
+const putPolicy = new qiniu.rs.PutPolicy(options)
 
 module.exports = {
   getQiniuToken: async (ctx, next) => {
     try {
-      const mac = new qiniu.auth.digest.Mac(accessKey, secretKey)
-      const putPolicy = new qiniu.rs.PutPolicy(options)
       const uploadToken = putPolicy.uploadToken(mac)
-
       ctx.send({status: 'ok', message: '七牛上传凭证创建成功', data: uploadToken})
     } catch (e) {
       ctx.send({status: 'no', message: '七牛上传凭证创建失败'})
