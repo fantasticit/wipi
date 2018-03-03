211b86a38e1c6cb0d9de89a81246aee67083dca9
zhaoxu <bken2016@163.com>
2018-03-03 23:00:12 +0800

    fix page article

--- fe/pages/article.js
+++ fe/pages/article.js
@@ -22,6 +22,7 @@ class Article extends Component {
     return(
       <Layout>
         <div className="container">
+          <h1>{ article.title }</h1>
           <Author author={{
             avatar: article.author.avatar,
             account: article.author.account,
@@ -37,6 +38,14 @@ class Article extends Component {
         <style jsx>{`
         .container {
           padding: 2rem 15px 3rem 15px;
+        }
+
+        h1 {
+          word-break: break-all;
+          font-family: -apple-system,SF UI Display,Arial,PingFang SC,Hiragino Sans GB,Microsoft YaHei,WenQuanYi Micro Hei,sans-serif;
+          font-size: 34px;
+          font-weight: 700;
+          line-height: 1.3;
         }
         `}</style>
       </Layout>
