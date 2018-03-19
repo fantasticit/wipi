fcf08298818fa70dfddf0679d0cca9d37dc81117
zhaoxu <bken2016@163.com>
2018-03-19 14:05:41 +0800

    Server: update

--- server-2.0.0/controllers/user.js
+++ server-2.0.0/controllers/user.js
@@ -5,61 +5,45 @@ module.exports = app => {
   const UserController = {}
 
   UserController.register = async ctx => {
-    try {
-      let { account, passwd } = ctx.request.body;
+    let { account, passwd } = ctx.request.body;
+    !passwd ? ctx.throw(400, { message: '请输入密码' }) : '';
+    passwd = app.service.encrypt(passwd);
 
-      if (!passwd) {
-        ctx.throw(400, { message: '请输入密码' });
+    const result = await model.create({ account, passwd }).catch(e => {
+      if (e.code === 11000) {
+        ctx.throw(400, { code: 'no', message: '用户已存在'});
+      } else {
+        ctx.throw(500);
       }
-
-      passwd = app.service.encrypt(passwd);
-
-      const result = await model.create({ account, passwd }).catch(e => {
-        if (e.code === 11000) {
-          ctx.throw(400, { code: 'no', message: '用户已存在'});
-        } else {
-          ctx.throw(500);
-        }
-      })
-      ctx.body = { code: 'ok', message: '注册成功', data: result };
-    } catch (err) {
-      ctx.body = err;
-      throw new Error(err);
-    }
+    })
+    ctx.body = { code: 'ok', message: '注册成功', data: result };
   }
 
   UserController.login = async ctx => {
-    try {
-      let { account, passwd } = ctx.request.body;
-      !passwd ? ctx.throw(400, { message: '请输入密码' }) : '';
-      passwd = app.service.encrypt(passwd);
+    let { account, passwd } = ctx.request.body;
+    !passwd ? ctx.throw(400, { message: '请输入密码' }) : '';
+    passwd = app.service.encrypt(passwd);
 
-      const user = await model.findOne({ account, passwd }).catch(e => ctx.throw(500))
+    const user = await model.findOne({ account, passwd }).catch(e => ctx.throw(500))
 
-      if (!!user) {
-        // 生成token
-        const token = jwt.sign(
-          { 
-            id: user._id,
-            account,
-            avatar: user.avatar,
-            roles: user.roles, 
-            lastLoginAt: user.lastLoginAt,
-            createAt: user.createAt
-          }, 
-          app.config.token.secret,
-          { expiresIn: app.config.token.expires }
-        )
-        ctx.body = ({ code: 'ok', message: `登录成功`, data: token })
-  
-        // 更新最后登录时间
-        await model.findByIdAndUpdate(user._id, { lastLoginAt: Date.now() })
-      } else {
-        ctx.throw(400, { message: `账号或密码错误` })
-      }
-    } catch (err) {
-      ctx.body = err;
-      throw new Error(err);
+    if (!!user) {
+      // 生成token
+      const token = jwt.sign({ 
+          id: user._id,
+          account,
+          avatar: user.avatar,
+          roles: user.roles, 
+          lastLoginAt: user.lastLoginAt,
+          createAt: user.createAt
+        }, 
+        app.config.token.secret,
+        { expiresIn: app.config.token.expires }
+      )
+      ctx.body = ({ code: 'ok', message: `登录成功`, data: token })
+      // 更新最后登录时间
+      await model.findByIdAndUpdate(user._id, { lastLoginAt: Date.now() })
+    } else {
+      ctx.throw(400, { message: `账号或密码错误` })
     }
   }
 
