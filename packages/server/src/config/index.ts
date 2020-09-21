e6efa70b9341ff7728ddf5e8c9610f29161f21e3
Fantasticit <zhxuc7@163.com>
2020-09-21 14:34:53 +0800

    Update index.ts

--- packages/server/src/config/index.ts
+++ packages/server/src/config/index.ts
@@ -2,7 +2,7 @@ export const config = {
   admin: { name: 'admin', password: 'admin' },
   mysql: {
     host: '0.0.0.0',
-    port: 33721,
+    port: 3306,
     username: 'root',
     password: 'root',
     database: 'wipi',
