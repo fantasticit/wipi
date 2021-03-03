d1354c6cc7ff5880f251b947d5f2f5189f53f55e
fantasticit <fantasticit@163.com>
2021-03-03 19:48:37 +0800

    fix: fix date format

--- packages/server/src/utils/date.util.ts
+++ packages/server/src/utils/date.util.ts
@@ -1,9 +1,10 @@
-import _dateFormat from 'date-fns/format';
+import * as _dateFormat from 'date-fns/format';
 
 export const dateFormat = (date = null, format = 'yyyy-MM-dd HH:mm:ss') => {
   if (date === null || date === undefined) {
     date = new Date();
   }
   const t = date instanceof Date ? date : new Date(date);
-  return dateFormat(t, format);
+  // @ts-ignore
+  return _dateFormat(t, format);
 };
