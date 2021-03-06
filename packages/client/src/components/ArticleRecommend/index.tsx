647a78a80db20367cffff83f9052f5eea2d4661a
fantasticit <fantasticit@163.com>
2021-03-06 21:24:31 +0800

    fix: fix empty, add loading

--- packages/client/src/components/ArticleRecommend/index.tsx
+++ packages/client/src/components/ArticleRecommend/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
-import { Icon } from 'antd';
+import { Icon, Spin } from 'antd';
 import { ArticleProvider } from '@/providers/article';
 import { ArticleList } from '@components/ArticleList';
 import { LocaleTime } from '@/components/LocaleTime';
@@ -17,12 +17,19 @@ export const ArticleRecommend: React.FC<IProps> = ({
   articleId = null,
   needTitle = true,
 }) => {
+  const [loading, setLoading] = useState(false);
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    ArticleProvider.getRecommend(articleId).then((res) => {
-      setArticles(res.slice(0, 6));
-    });
+    setLoading(true);
+    ArticleProvider.getRecommend(articleId)
+      .then((res) => {
+        setArticles(res.slice(0, 6));
+        setLoading(false);
+      })
+      .catch(() => {
+        setLoading(false);
+      });
   }, [articleId]);
 
   return (
@@ -33,35 +40,37 @@ export const ArticleRecommend: React.FC<IProps> = ({
           <span>推荐文章</span>
         </div>
       )}
-      {mode === 'inline' ? (
-        articles.length <= 0 ? (
-          <p className={style.empty}>暂无推荐</p>
+      <Spin spinning={loading}>
+        {mode === 'inline' ? (
+          articles.length <= 0 ? (
+            <div className={'empty'}>{!loading && '暂无推荐'}</div>
+          ) : (
+            <ul className={style.inlineWrapper}>
+              {(articles || []).map((article) => {
+                return (
+                  <li key={article.id} className={style.inlineItem}>
+                    <div>
+                      <Link href={`/article/[id]`} as={`/article/${article.id}`} scroll={false}>
+                        <a>
+                          <p className={style.articleTitle}>
+                            <span>{article.title}</span>
+                            {' · '}
+                            <span>
+                              <LocaleTime date={article.publishAt} timeago />
+                            </span>
+                          </p>
+                        </a>
+                      </Link>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          )
         ) : (
-          <ul className={style.inlineWrapper}>
-            {(articles || []).map((article) => {
-              return (
-                <li key={article.id} className={style.inlineItem}>
-                  <div>
-                    <Link href={`/article/[id]`} as={`/article/${article.id}`} scroll={false}>
-                      <a>
-                        <p className={style.articleTitle}>
-                          <span>{article.title}</span>
-                          {' · '}
-                          <span>
-                            <LocaleTime date={article.publishAt} timeago />
-                          </span>
-                        </p>
-                      </a>
-                    </Link>
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
-        )
-      ) : (
-        <ArticleList articles={articles || []} />
-      )}
+          <ArticleList articles={articles || []} />
+        )}
+      </Spin>
     </div>
   );
 };
