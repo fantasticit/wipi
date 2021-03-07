5462af256bd8fb7027d86ef7418c7222cf159837
fantasticit <fantasticit@163.com>
2021-03-07 20:53:14 +0800

    fix: fix ViewStatistics

--- packages/admin/src/components/ViewStatistics.tsx
+++ packages/admin/src/components/ViewStatistics.tsx
@@ -1,17 +1,21 @@
 import { useEffect } from 'react';
 import { ViewProvider } from '@/providers/view';
 
+let lastUrl;
+
 const addView = (url) => {
   if (/localhost|127\.0\.0\.1/.test(url)) {
     return;
   }
+  if (lastUrl === url) return;
+  lastUrl = url;
   ViewProvider.addView({ url });
 };
 
 export const ViewStatistics = (props) => {
   useEffect(() => {
     addView(window.location.href);
-  }, []);
+  });
 
   return props.children || null;
 };
