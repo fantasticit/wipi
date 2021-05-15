5c436410c300febfa9a1284ee999d87b78a24371
fantasticit <fantasticit@163.com>
2021-05-15 22:51:51 +0800

    feat(admin): update layout

--- packages/admin/src/layout/AdminLayout/index.tsx
+++ packages/admin/src/layout/AdminLayout/index.tsx
@@ -1,89 +1,88 @@
-import React from 'react';
-import { Layout, Breadcrumb, Menu, Icon } from 'antd';
+import React, { useContext } from 'react';
+import { Layout, Breadcrumb, Row, Col, Menu, Icon } from 'antd';
 import Link from 'next/link';
 import cls from 'classnames';
 import { useSetting } from '@/hooks/useSetting';
 import Router, { useRouter } from 'next/router';
 import { UserInfo } from '@/components/UserInfo';
-import { nestMenus, findActiveMenu, findNestMenuChildren } from './menus';
+import { GlobalContext } from '@/context/global';
+import { ResourceCreate } from './ResourceCreate';
+import { menus, findActiveMenu } from './menus';
 import style from './index.module.scss';
 
-const { Sider, Header, Content, Footer } = Layout;
+const { Sider, Content } = Layout;
 
 export const AdminLayout: React.FC = ({ children }) => {
+  const { collapsed, toggleCollapse } = useContext(GlobalContext);
   const setting = useSetting();
   const router = useRouter();
   const { pathname } = router;
   const [activeMenu, breadcrumbs] = findActiveMenu(pathname);
-  const submenus = findNestMenuChildren(pathname);
 
   return (
     <Layout className={style.container}>
-      <Header className={style.header}>
+      <Sider className={style.asider} trigger={null} collapsible={true} collapsed={collapsed}>
         <div className={style.logo}>
           {setting.systemFavicon && <img src={setting.systemFavicon} />}
-          <span style={{ marginLeft: 4 }}>管理后台</span>
+          {!collapsed && <span style={{ marginLeft: 4 }}>管理后台</span>}
         </div>
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={[activeMenu && activeMenu.path]}
-          style={{ lineHeight: '64px' }}
-        >
-          {nestMenus.map((menu) => {
-            return (
-              <Menu.Item key={menu.path} onClick={() => Router.push(menu.path)}>
-                <Link href={menu.path}>
-                  <a
-                    className={cls({
-                      [style.active]: activeMenu && activeMenu.path === menu.path,
-                    })}
-                  >
-                    <Icon type={menu.icon} />
-                    <span>{menu.label}</span>
-                  </a>
-                </Link>
-              </Menu.Item>
-            );
-          })}
-          <Menu.Item>
-            <UserInfo />
-          </Menu.Item>
+        <div className={style.resourceCreate}>
+          <ResourceCreate collapsed={collapsed} />
+        </div>
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={[activeMenu && activeMenu.path]}>
+          {menus
+            .filter((m: any) => !m.ignore)
+            .map((menu) => {
+              return menu.divider ? (
+                <div className={style.divider}></div>
+              ) : (
+                <Menu.Item key={menu.path} onClick={() => Router.push(menu.path)}>
+                  <Link href={menu.path}>
+                    <a
+                      className={cls({
+                        [style.active]: activeMenu && activeMenu.path === menu.path,
+                      })}
+                    >
+                      <Icon type={menu.icon} />
+                      <span>{menu.label}</span>
+                    </a>
+                  </Link>
+                </Menu.Item>
+              );
+            })}
         </Menu>
-      </Header>
-      <Content className={style.main}>
-        <Layout className={style.content}>
-          {submenus.length ? (
-            <Sider width={160} className={style.aside}>
-              <Menu
-                mode="inline"
-                defaultSelectedKeys={['1']}
-                defaultOpenKeys={[activeMenu && activeMenu.path]}
-              >
-                {submenus.map((menu) => {
-                  return (
-                    <Menu.Item key={menu.path} onClick={() => Router.push(menu.path)}>
-                      <Link href={menu.path}>
-                        <a
-                          className={cls({
-                            [style.active]: activeMenu && activeMenu.path === menu.path,
-                          })}
-                        >
-                          <Icon type={menu.icon} />
-                          <span>{menu.label}</span>
-                        </a>
-                      </Link>
-                    </Menu.Item>
-                  );
-                })}
-              </Menu>
-            </Sider>
-          ) : null}
-          <Layout style={{ paddingLeft: submenus.length ? 24 : 0 }}>
-            <Breadcrumb className={style.breadcrumbs}>
+      </Sider>
+      <Layout className={style.main}>
+        <header>
+          <Row>
+            <Col span={12}>
+              <Icon
+                className="trigger"
+                type={collapsed ? 'menu-unfold' : 'menu-fold'}
+                onClick={toggleCollapse}
+              />
+            </Col>
+            <Col span={12} style={{ textAlign: 'right' }}>
+              <div className={style.info}>
+                <a
+                  className={style.github}
+                  href="https://github.com/fantasticit/wipi"
+                  target="_blank"
+                >
+                  <Icon type="github" />
+                  <span></span>
+                </a>
+                <UserInfo />
+              </div>
+            </Col>
+          </Row>
+        </header>
+        <Content className={style.content}>
+          <header>
+            <Breadcrumb>
               {breadcrumbs.map((breadcrumb) => {
                 return (
-                  <Breadcrumb.Item key={breadcrumb.path}>
+                  <Breadcrumb.Item>
                     <Link href={breadcrumb.path}>
                       <a>{breadcrumb.title}</a>
                     </Link>
@@ -91,33 +90,35 @@ export const AdminLayout: React.FC = ({ children }) => {
                 );
               })}
             </Breadcrumb>
-            <Content>{children}</Content>
-          </Layout>
-        </Layout>
-      </Content>
-      <Footer className={style.footer}>
-        <ul className={style.icons}>
-          <li>
-            <a
-              className={style.github}
-              href="https://github.com/fantasticit/wipi"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <Icon type="github" />
-            </a>
-          </li>
-        </ul>
-        <div className={style.copyright}>
-          <p>
-            Copyright <Icon type="copyright" /> {new Date().getFullYear()} Designed by
-            <a href="https://github.com/fantasticit/wipi" target="_blank" rel="noreferrer">
-              {' '}
-              Fantasticit.
-            </a>
-          </p>
-        </div>
-      </Footer>
+            <div className={style.title}>{activeMenu && activeMenu.label}</div>
+          </header>
+          <main>
+            {children}
+            <footer>
+              <ul className={style.icons}>
+                <li>
+                  <a
+                    className={style.github}
+                    href="https://github.com/fantasticit/wipi"
+                    target="_blank"
+                  >
+                    <Icon type="github" />
+                  </a>
+                </li>
+              </ul>
+              <div className={style.copyright}>
+                <p>
+                  Copyright <Icon type="copyright" /> {new Date().getFullYear()} Designed by
+                  <a href="https://github.com/fantasticit/wipi" target="_blank">
+                    {' '}
+                    Fantasticit.
+                  </a>
+                </p>
+              </div>
+            </footer>
+          </main>
+        </Content>
+      </Layout>
     </Layout>
   );
 };
