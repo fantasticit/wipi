6bd251bd7fc54983fc528f18078af14a7c1809e4
fantasticit <fantasticit@163.com>
2021-05-28 09:32:50 +0800

    feat(admin): update Editor

--- packages/admin/pages/knowledge/editor/[id].tsx
+++ packages/admin/pages/knowledge/editor/[id].tsx
@@ -20,6 +20,211 @@ interface IProps {
   knowledge: Partial<IKnowledge>;
 }
 
+// const Page: NextPage<IProps> = ({ id, knowledge }) => {
+//   const forceUpdate = useForceUpdate();
+//   const [loading, setLoading] = useState(false);
+//   const [popVisible, togglePopVisible] = useToggle(false);
+//   const [fileVisible, toggleFileVisible] = useToggle(false);
+//   const [newTitle, setNewTitle] = useState('');
+//   const [currentIndex, setCurrentIndex] = useState(-1);
+//   const [chapters, setChapters] = useState<Array<Partial<IKnowledge>>>(knowledge.children || []);
+//   const currentChapter = chapters[currentIndex] || null;
+
+//   const deleteKnowledge = useCallback(
+//     (idx) => {
+//       const handle = () => {
+//         setChapters((chapters) => {
+//           chapters.splice(idx, 1);
+//           return chapters;
+//         });
+//         forceUpdate();
+//         setCurrentIndex(currentIndex - 1);
+//       };
+//       const target = chapters[idx];
+//       if (target.id) {
+//         KnowledgeProvider.deleteKnowledge(target.id).then(() => {
+//           handle();
+//           message.success('已保存');
+//         });
+//       } else {
+//         handle();
+//       }
+//     },
+//     [chapters, currentIndex, forceUpdate]
+//   );
+
+//   const SortableItem = SortableElement(({ value: idx }) => (
+//     <li
+//       key={idx}
+//       className={cls({ [styles.active]: idx === currentIndex, [styles.item]: true })}
+//       onClick={() => setCurrentIndex(idx)}
+//     >
+//       <DragHandle />
+//       <span>{chapters[idx].title}</span>
+//       <Popconfirm
+//         title="确认删除?"
+//         onConfirm={() => deleteKnowledge(idx)}
+//         okText="确定"
+//         cancelText="取消"
+//       >
+//         <DeleteOutlined onClick={(e) => e.stopPropagation()} />
+//       </Popconfirm>
+//     </li>
+//   ));
+
+//   const SortableList = SortableContainer(({ items }) => {
+//     return (
+//       <ul className={styles.menu}>
+//         {items.map((_, index) => (
+//           <SortableItem key={`item-${index}`} index={index} value={index} />
+//         ))}
+//       </ul>
+//     );
+//   });
+
+//   const onSortEnd = useCallback(({ oldIndex, newIndex }) => {
+//     setChapters((chapters) => {
+//       return arrayMove(chapters, oldIndex, newIndex);
+//     });
+//   }, []);
+
+//   const createNewKnowledge = useCallback(() => {
+//     const title = newTitle.trim();
+//     if (!title) {
+//       return;
+//     }
+//     setChapters((chapters) => {
+//       chapters.push({
+//         title: title,
+//         content: '',
+//       });
+//       return chapters;
+//     });
+//     setCurrentIndex(chapters.length - 1);
+//     setNewTitle('');
+//     togglePopVisible();
+//     forceUpdate();
+//   }, [newTitle, chapters, forceUpdate, togglePopVisible]);
+
+//   const patchKnowledge = useCallback(
+//     (patch) => {
+//       if (currentIndex < 0) {
+//         return;
+//       }
+//       setChapters((chapters) => {
+//         const target = chapters[currentIndex];
+//         if (!target) {
+//           return chapters;
+//         }
+//         target.content = patch.value;
+//         target.html = patch.html;
+//         target.toc = patch.toc;
+//         return chapters;
+//       });
+//     },
+//     [currentIndex]
+//   );
+
+//   const save = useCallback(() => {
+//     if (!chapters || !chapters.length) {
+//       return;
+//     }
+//     chapters.forEach((chapter, idx) => {
+//       chapter.order = idx;
+//     });
+//     setLoading(true);
+//     const promises = chapters.map((chapter) => {
+//       if (chapter.parentId) {
+//         return KnowledgeProvider.updateKnowledge(chapter.id, chapter);
+//       }
+//       return KnowledgeProvider.createChapters([{ ...chapter, parentId: id }]);
+//     });
+//     Promise.all(promises as Array<Promise<IKnowledge>>).then((res) => {
+//       const data = res.flat(Infinity);
+//       setChapters(data);
+//       forceUpdate();
+//       setLoading(false);
+//     });
+//   }, [id, chapters, forceUpdate]);
+
+//   return (
+//     <div className={styles.wrap}>
+//       <aside className={styles.aside}>
+//         <header>
+//           <div>
+//             <Avatar shape="square" size="large" src={knowledge.cover} />
+//             <span style={{ marginLeft: 8 }}>{knowledge.title}</span>
+//           </div>
+//           <Popconfirm
+//             title="确认关闭？如果有内容变更，请先保存。"
+//             onConfirm={() => Router.push('/knowledge')}
+//             onCancel={() => null}
+//             okText="确认"
+//             cancelText="取消"
+//             placement="rightBottom"
+//           >
+//             <CloseOutlined />
+//           </Popconfirm>
+//         </header>
+//         <Divider type="horizontal" />
+//         <main>
+//           {chapters.length > 0 ? (
+//             <div className={cls(styles.action, styles.saveAction)}>
+//               <span>{chapters.length}篇文章</span>
+//               <Button size="small" type="primary" onClick={save} loading={loading}>
+//                 保存
+//               </Button>
+//             </div>
+//           ) : null}
+//           <Popover
+//             content={
+//               <div style={{ display: 'flex' }}>
+//                 <Input
+//                   autoFocus={true}
+//                   width={240}
+//                   value={newTitle}
+//                   onChange={(e) => setNewTitle(e.target.value)}
+//                 />
+//                 <Button style={{ marginLeft: 8 }} type="primary" onClick={createNewKnowledge}>
+//                   新建
+//                 </Button>
+//               </div>
+//             }
+//             visible={popVisible}
+//             onVisibleChange={togglePopVisible}
+//             placement="rightTop"
+//             trigger="click"
+//           >
+//             <div className={styles.action}>
+//               <span>新建</span>
+//               <PlusOutlined />
+//             </div>
+//           </Popover>
+//           <div className={styles.action} onClick={toggleFileVisible}>
+//             <span>文件</span>
+//             <FolderOutlined />
+//           </div>
+//           <FileSelectDrawer isCopy={true} visible={fileVisible} onClose={toggleFileVisible} />
+//         </main>
+//         <Divider type="horizontal" />
+//         <footer>
+//           <SortableList items={chapters} onSortEnd={onSortEnd} useDragHandle={true} />
+//         </footer>
+//       </aside>
+//       <main className={styles.main}>
+//         {currentChapter ? (
+//           <Editor
+//             defaultValue={(currentChapter && currentChapter.content) || ''}
+//             onChange={patchKnowledge}
+//           />
+//         ) : (
+//           <div className={styles.helper}>请新建章节（或者选择章节进行编辑）</div>
+//         )}
+//       </main>
+//     </div>
+//   );
+// };
+
 const Page: NextPage<IProps> = ({ id, knowledge }) => {
   const forceUpdate = useForceUpdate();
   const [loading, setLoading] = useState(false);
@@ -148,13 +353,9 @@ const Page: NextPage<IProps> = ({ id, knowledge }) => {
   }, [id, chapters, forceUpdate]);
 
   return (
-    <div className={styles.wrap}>
-      <aside className={styles.aside}>
-        <header>
-          <div>
-            <Avatar shape="square" size="large" src={knowledge.cover} />
-            <span style={{ marginLeft: 8 }}>{knowledge.title}</span>
-          </div>
+    <div className={styles.wrapper}>
+      <header>
+        <div>
           <Popconfirm
             title="确认关闭？如果有内容变更，请先保存。"
             onConfirm={() => Router.push('/knowledge')}
@@ -163,63 +364,59 @@ const Page: NextPage<IProps> = ({ id, knowledge }) => {
             cancelText="取消"
             placement="rightBottom"
           >
-            <CloseOutlined />
+            <Button size="small" icon={<CloseOutlined />} style={{ marginRight: 12 }} />
           </Popconfirm>
-        </header>
-        <Divider type="horizontal" />
-        <main>
-          {chapters.length > 0 ? (
-            <div className={cls(styles.action, styles.saveAction)}>
-              <span>{chapters.length}篇文章</span>
-              <Button size="small" type="primary" onClick={save} loading={loading}>
-                保存
+          <Avatar shape="square" src={knowledge.cover} />
+          <span style={{ marginLeft: 8 }}>{knowledge.title}</span>
+        </div>
+        <div>
+          <Button type="primary" onClick={save} loading={loading}>
+            保存
+          </Button>
+        </div>
+      </header>
+      <main>
+        <aside>
+          <header>
+            <span>{chapters.length}篇文章</span>
+            <Popover
+              content={
+                <div style={{ display: 'flex' }}>
+                  <Input
+                    autoFocus={true}
+                    width={240}
+                    value={newTitle}
+                    onChange={(e) => setNewTitle(e.target.value)}
+                  />
+                  <Button style={{ marginLeft: 8 }} type="primary" onClick={createNewKnowledge}>
+                    新建
+                  </Button>
+                </div>
+              }
+              visible={popVisible}
+              onVisibleChange={togglePopVisible}
+              placement="rightTop"
+              trigger="click"
+            >
+              <Button icon={<PlusOutlined />} size="small">
+                新建
               </Button>
-            </div>
-          ) : null}
-          <Popover
-            content={
-              <div style={{ display: 'flex' }}>
-                <Input
-                  autoFocus={true}
-                  width={240}
-                  value={newTitle}
-                  onChange={(e) => setNewTitle(e.target.value)}
-                />
-                <Button style={{ marginLeft: 8 }} type="primary" onClick={createNewKnowledge}>
-                  新建
-                </Button>
-              </div>
-            }
-            visible={popVisible}
-            onVisibleChange={togglePopVisible}
-            placement="rightTop"
-            trigger="click"
-          >
-            <div className={styles.action}>
-              <span>新建</span>
-              <PlusOutlined />
-            </div>
-          </Popover>
-          <div className={styles.action} onClick={toggleFileVisible}>
-            <span>文件</span>
-            <FolderOutlined />
-          </div>
-          <FileSelectDrawer isCopy={true} visible={fileVisible} onClose={toggleFileVisible} />
-        </main>
-        <Divider type="horizontal" />
-        <footer>
-          <SortableList items={chapters} onSortEnd={onSortEnd} useDragHandle={true} />
-        </footer>
-      </aside>
-      <main className={styles.main}>
-        {currentChapter ? (
-          <Editor
-            defaultValue={(currentChapter && currentChapter.content) || ''}
-            onChange={patchKnowledge}
-          />
-        ) : (
-          <div className={styles.helper}>请新建章节（或者选择章节进行编辑）</div>
-        )}
+            </Popover>
+          </header>
+          <main>
+            <SortableList items={chapters} onSortEnd={onSortEnd} useDragHandle={true} />
+          </main>
+        </aside>
+        <section>
+          {currentChapter ? (
+            <Editor
+              defaultValue={(currentChapter && currentChapter.content) || ''}
+              onChange={patchKnowledge}
+            />
+          ) : (
+            <div className={styles.helper}>请新建章节（或者选择章节进行编辑）</div>
+          )}
+        </section>
       </main>
     </div>
   );
