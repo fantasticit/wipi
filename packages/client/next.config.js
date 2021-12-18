dd6d9120881c74a2a3a59c9cb6db6323e78a29cd
fantasticit <fantasticit@163.com>
2021-12-18 22:08:42 +0800

    feat(client): fix textarea not return html event

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -35,11 +35,11 @@ const nextConfig = {
     config.node.fs = 'empty';
 
     if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        'react': 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      });
+      // Object.assign(config.resolve.alias, {
+      //   'react': 'preact/compat',
+      //   'react-dom/test-utils': 'preact/test-utils',
+      //   'react-dom': 'preact/compat',
+      // });
     }
 
     return config;
