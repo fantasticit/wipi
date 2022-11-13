932617903b161c97c6c4ea4b91c1fa417abc5a01
UnrilWever <641321483@qq.com>
2022-11-13 18:38:13 +0800

    Feat:New tags bar

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -2,6 +2,7 @@ import { ArticleCarousel } from '@components/ArticleCarousel';
 import { ArticleList } from '@components/ArticleList';
 import { Footer } from '@components/Footer';
 import { Tags } from '@components/Tags';
+import { Menu } from 'antd';
 import cls from 'classnames';
 import { NextPage } from 'next';
 import Link from 'next/link';
@@ -16,22 +17,18 @@ import { DoubleColumnLayout } from '@/layout/DoubleColumnLayout';
 import { ArticleProvider } from '@/providers/article';
 
 import style from './index.module.scss';
-
 interface IHomeProps {
   articles: IArticle[];
   total: number;
   recommendedArticles: IArticle[];
 }
-
 const pageSize = 12;
-
 export const CategoryMenu = ({ categories }) => {
   const t = useTranslations();
   const router = useRouter();
   const { asPath } = router;
-
   return (
-    <>
+    <Menu mode="horizontal">
       {[
         {
           label: t('all'),
@@ -39,41 +36,39 @@ export const CategoryMenu = ({ categories }) => {
         },
         ...categories,
       ].map((category, index) => (
-        <Link
+        <Menu.Item
           key={index}
-          {...(index === 0
-            ? { href: '/' }
-            : {
-                href: '/category/[category]',
-                as: `/category/` + category.value,
-              })}
-          shallow={false}
+          className={cls({
+            ['ant-menu-item-selected']:
+              index === 0 ? asPath === category.path : asPath.replace('/category/', '') === category.value,
+          })}
         >
-          <a
-            className={cls({
-              [style.active]:
-                index === 0 ? asPath === category.path : asPath.replace('/category/', '') === category.value,
-            })}
-            aria-label={category.label}
+          <Link
+            {...(index === 0
+              ? { href: '/' }
+              : {
+                  href: '/category/[category]',
+                  as: `/category/` + category.value,
+                })}
+            shallow={false}
           >
-            <span>{category.label}</span>
-          </a>
-        </Link>
+            <a aria-label={category.label}>
+              <span>{category.label}</span>
+            </a>
+          </Link>
+        </Menu.Item>
       ))}
-    </>
+    </Menu>
   );
 };
-
 const Home: NextPage<IHomeProps> = ({ articles: defaultArticles = [], recommendedArticles = [], total = 0 }) => {
   const t = useTranslations();
   const { setting, tags, categories } = useContext(GlobalContext);
   const [page, setPage] = useState(1);
   const [articles, setArticles] = useState<IArticle[]>(defaultArticles);
-
   useEffect(() => {
     setArticles(defaultArticles);
   }, [defaultArticles]);
-
   const getArticles = useCallback((page) => {
     ArticleProvider.getArticles({
       page,
@@ -84,7 +79,6 @@ const Home: NextPage<IHomeProps> = ({ articles: defaultArticles = [], recommende
       setArticles((articles) => [...articles, ...res[0]]);
     });
   }, []);
-
   return (
     <div className={style.wrapper}>
       <DoubleColumnLayout
@@ -127,7 +121,6 @@ const Home: NextPage<IHomeProps> = ({ articles: defaultArticles = [], recommende
     </div>
   );
 };
-
 // 服务端预取数据
 Home.getInitialProps = async () => {
   const [articles, recommendedArticles] = await Promise.all([
@@ -141,5 +134,4 @@ Home.getInitialProps = async () => {
     needLayoutFooter: false,
   };
 };
-
 export default Home;
